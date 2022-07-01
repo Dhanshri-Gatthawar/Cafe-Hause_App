09ca84edc8763be583f9a60e95133db0e3399db7
Dhanshri Gatthawar <2020bcs106@sggs.ac.in>
2022-07-01 17:49:22 +0530

    Cafe House App

--- app/src/main/java/com/example/cafehousemyown/MainActivity.java
+++ app/src/main/java/com/example/cafehousemyown/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cafehousemyown;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
+
+        Intent intent = new Intent(MainActivity.this,Sign_Up.class);
+
     }
 }
