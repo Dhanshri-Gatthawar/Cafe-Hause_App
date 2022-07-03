dddfb4661c6931dea350c6023a859f144dd076c6
Dhanshri Gatthawar <2020bcs106@sggs.ac.in>
2022-07-03 16:32:24 +0530

    Cafe House App

--- app/src/main/java/com/example/cafehousemyown/MainActivity.java
+++ app/src/main/java/com/example/cafehousemyown/MainActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
         Intent intent= new Intent(MainActivity.this,Sign_Up.class);
+
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
 
     }
 }
